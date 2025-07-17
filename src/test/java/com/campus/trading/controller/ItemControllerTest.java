43ce9fff162f871851d46ba6d5ee83b379d6c896
523031910511 <yangzixi @sjtu.edu.cn>
2025-07-17 16:36:02 +0800

    更新vision

---
+++ src/test/java/com/campus/trading/controller/ItemControllerTest.java
@@ -0,0 +1,64 @@
+package com.campus.trading.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemControllerTest {
+
+    @Test
+    void createItem() {
+    }
+
+    @Test
+    void updateItem() {
+    }
+
+    @Test
+    void getItemById() {
+    }
+
+    @Test
+    void publishItem() {
+    }
+
+    @Test
+    void unpublishItem() {
+    }
+
+    @Test
+    void listItems() {
+    }
+
+    @Test
+    void listUserItems() {
+    }
+
+    @Test
+    void listCategoryItems() {
+    }
+
+    @Test
+    void searchItems() {
+    }
+
+    @Test
+    void generateItemDescription() {
+    }
+
+    @Test
+    void getRecommendedItems() {
+    }
+
+    @Test
+    void getPlatformStatistics() {
+    }
+
+    @Test
+    void incrementPopularity() {
+    }
+
+    @Test
+    void getPopularity() {
+    }
+}
