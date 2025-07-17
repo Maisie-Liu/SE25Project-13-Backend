de5f2b83e0a4e71a972984893a5c0fbecd9574a2
Maisie-Liu <1014889214@qq.com>
2025-07-18 01:00:50 +0800

    删除FIXME

--- src/test/java/com/campus/trading/service/impl/MessageServiceImplTest.java
+++ src/test/java/com/campus/trading/service/impl/MessageServiceImplTest.java
@@ -23,7 +23,6 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
-// FIXME
 @SpringBootTest
 @ActiveProfiles("test")
 class MessageServiceImplTest {
